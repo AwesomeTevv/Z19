70487ebba7a5a46b521bdadab10037222bd57c60
AwesomeTevv <tevlen.naidoo@gmail.com>
2025-08-04 15:42:59 +0200

    Math helper file

---
+++ src/helpers/mathHelpers.js
@@ -0,0 +1,8 @@
+/**
+ * Function that converts an angle in degrees to radians
+ * @param {Number} degrees Angle in degrees
+ * @returns Angle in radians
+ */
+export function toRadians(degrees) {
+    return (degrees * Math.PI) / 180;
+}
