3611e1e159e04a3253bf474f0c37c19973a83809
AwesomeTevv <tevlen.naidoo@gmail.com>
2025-08-05 23:38:24 +0200

    Tired

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        scene: resolve(__dirname, 'scene.html')
+      }
+    }
+  },
   server: {
     host: '0.0.0.0',
     port: 3000
